e52abe300acb1c88aaf5483c096b98dc316e5ecd
baifang.xing <baifang.xing@qunar.com>
2017-12-23 23:34:40 +0800

    init

---
+++ controller/webhook.js
@@ -0,0 +1,31 @@
+const Koa = require('koa');
+const urllib = require('urllib');
+
+class WebhookController {
+    async index(ctx) {
+
+        // Dialogflow api V1
+        const { result = {} } = ctx.request.body;
+        const { action, parameters, inputContexts } = result;
+
+        const response = {};
+
+        if (action === 'qunarWeather') {
+            const city = parameters['geo-city'];
+            if(city) {
+                // 天气查询
+                // http://qapi.qunar.com/api/weather/current?city=%E5%8C%97%E4%BA%AC
+                const requestResult = await urllib.request(`http://qapi.qunar.com/api/weather/current?city=${city}`);
+                if (requestResult.data && requestResult.data.status === 0) {
+                    const weatherData = requestResult.data.data;
+                    const speech = `今天${city}天气${weatherData.symbol}.`;
+                    response.speech = response.displayText = speech;
+                }
+            }
+        }
+        
+        ctx.body = response;
+    }
+};
+
+module.exports = WebhookController;
---
+++ index.js
@@ -0,0 +1,18 @@
+const Koa = require('koa');
+const app = new Koa();
+const router = require('koa-router')();
+const koaBody = require('koa-body');
+const PORT = 3000;
+const WebhookController = new (require('./controller/webhook'));
+
+app.use(router.routes());
+
+router.post('/webhook', koaBody(), WebhookController.index);
+
+app.on('error', (err, ctx) => {
+    console.error('server error', err, ctx);
+});
+
+app.listen(PORT, () => {
+    console.log(`Please visited http://127.0.0.1:${PORT}`);
+});
