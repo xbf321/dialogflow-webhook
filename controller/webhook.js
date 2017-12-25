afca4adfec453f4a93e27cc5a2388cf072e02408
baifang.xing <baifang.xing@qunar.com>
2017-12-25 15:05:02 +0800

    add baidu weather

--- controller/webhook.js
+++ controller/webhook.js
@@ -6,9 +6,6 @@ class WebhookController {
         // Dialogflow api V1
         const { result = {} } = ctx.request.body;
         const { action, parameters, inputContexts } = result;
-
-        console.info(ctx.request.body);
-
         /**
          *    result: 
                 { source: 'agent',
@@ -29,28 +26,24 @@ class WebhookController {
                 sessionId: 'ecaa5125-f322-49cc-9cb4-f7f7a48aacdb' }
          */
         const response = {};
-
         if (action === 'qunarWeather') {
             const city = parameters['geo-city'];
             if(city) {
                 // 天气查询
-                // 
-                // const qunarWeatherResult =  await urllib.request(`http://qapi.qunar.com/api/weather/current?city=%E5%8C%97%E4%BA%AC`);
-                // const requestResult = await urllib.request(`http://php.weather.sina.com.cn/xml.php?city=%B1%B1%BE%A9&password=DJOYnieT8234jlsK&day=0`);
+                // https://www.heweather.com/documents/api/s6/weather-now
                 
-                const requestResult = await urllib.request(`http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=E4805d16520de693a3fe707cdc962045`);
+                const requestResult = await urllib.request(`https://free-api.heweather.com/s6/weather/now?key=b8f5102bfd89486d99555f4bba87739a&location=${encodeURIComponent(city)}`);
 
-                console.info(requestResult);
-                if (requestResult.data && requestResult.data.error === 0) {
-                    const weatherData = requestResult.results[0];
-                    const currentWeather = weatherData.weather_data[0];
-                    const speech = `今天${city}天气${currentWeather.weather},${currentWeather.wind},${currentWeather.temperature},PM25值${weatherData.pm25}.`;
-                    response.speech = response.displayText = speech;
-                    
+                if (requestResult.data && requestResult.data.HeWeather6.length > 0) {
+                    const heWeatherData = requestResult.data.HeWeather6[0];
+                    if (heWeatherData.status === 'ok') {
+                        const nowWeather = heWeatherData.now;
+                        const speech = `今天${city}天气${nowWeather.cond_text},${nowWeather.wind_dir}${nowWeather.wind_sc},温度${nowWeather.tmp}摄氏度.`;
+                        response.speech = response.displayText = speech;
+                    }
                 }
             }
         }
-        
         ctx.body = response;
         console.log(response);
     }
