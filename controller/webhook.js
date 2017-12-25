f2a1892633a00d6dd89941ba845e2a0a9e09b93c
baifang.xing <baifang.xing@qunar.com>
2017-12-25 15:11:08 +0800

    add baidu weather

--- controller/webhook.js
+++ controller/webhook.js
@@ -6,6 +6,7 @@ class WebhookController {
         // Dialogflow api V1
         const { result = {} } = ctx.request.body;
         const { action, parameters, inputContexts } = result;
+        const response = {};
         /**
          *    result: 
                 { source: 'agent',
@@ -25,20 +26,21 @@ class WebhookController {
                     status: { code: 200, errorType: 'success', webhookTimedOut: false },
                 sessionId: 'ecaa5125-f322-49cc-9cb4-f7f7a48aacdb' }
          */
-        const response = {};
         if (action === 'qunarWeather') {
             const city = parameters['geo-city'];
             if(city) {
                 // 天气查询
                 // https://www.heweather.com/documents/api/s6/weather-now
                 
-                const requestResult = await urllib.request(`https://free-api.heweather.com/s6/weather/now?key=b8f5102bfd89486d99555f4bba87739a&location=${encodeURIComponent(city)}`);
+                const requestResult = await urllib.request(`https://free-api.heweather.com/s6/weather/now?key=b8f5102bfd89486d99555f4bba87739a&location=${encodeURIComponent(city)}`, {
+                    dataType: 'json'
+                });
 
                 if (requestResult.data && requestResult.data.HeWeather6.length > 0) {
                     const heWeatherData = requestResult.data.HeWeather6[0];
                     if (heWeatherData.status === 'ok') {
                         const nowWeather = heWeatherData.now;
-                        const speech = `今天${city}天气${nowWeather.cond_text},${nowWeather.wind_dir}${nowWeather.wind_sc},温度${nowWeather.tmp}摄氏度.`;
+                        const speech = `今天${city}天气${nowWeather.cond_txt},${nowWeather.wind_dir}${nowWeather.wind_sc}级,温度${nowWeather.tmp}摄氏度.`;
                         response.speech = response.displayText = speech;
                     }
                 }
