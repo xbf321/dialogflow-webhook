663bb176e4926bd8dd8bb3f79d9afab6b662e808
baifang.xing <baifang.xing@qunar.com>
2017-12-25 16:26:10 +0800

    update

--- functions/weather.js
+++ functions/weather.js
@@ -1,4 +1,30 @@
 const urllib = require('urllib');
+const KEY = 'b8f5102bfd89486d99555f4bba87739a';
+const HOST = 'https://free-api.heweather.com';
+
+async function getPM25(city) {
+    const data = await fetch(`${HOST}/s6/air/now?key=${KEY}&location=${(city)}`);
+    return data.air_now_city || {};
+}
+
+async function getNow(city) {
+    const data = await fetch(`${HOST}/s6/weather/now?key=${KEY}&location=${(city)}`);
+    return data.now || {};
+}
+
+async function fetch(url) {
+    const requestResult = await urllib.request(url, {
+        dataType: 'json'
+    });
+
+    if (requestResult.data && requestResult.data.HeWeather6.length > 0) {
+        const heWeatherData = requestResult.data.HeWeather6[0];
+        if (heWeatherData.status === 'ok') {
+            return heWeatherData;
+        }
+    }
+    return {};
+}
 module.exports = async (result, response) => {
     const { parameters } = result;
     const city = parameters['geo-city'];
@@ -6,17 +32,12 @@ module.exports = async (result, response) => {
         // 天气查询
         // https://www.heweather.com/documents/api/s6/weather-now
 
-        const requestResult = await urllib.request(`https://free-api.heweather.com/s6/weather/now?key=b8f5102bfd89486d99555f4bba87739a&location=${encodeURIComponent(city)}`, {
-            dataType: 'json'
-        });
+        const weatherData = await getNow(encodeURIComponent(city));
+        const pm25Data = await getPM25(encodeURIComponent(city));
 
-        if (requestResult.data && requestResult.data.HeWeather6.length > 0) {
-            const heWeatherData = requestResult.data.HeWeather6[0];
-            if (heWeatherData.status === 'ok') {
-                const nowWeather = heWeatherData.now;
-                const speech = `今天${city}天气${nowWeather.cond_txt},${nowWeather.wind_dir}${nowWeather.wind_sc}级,温度${nowWeather.tmp}摄氏度.`;
-                response.speech = response.displayText = speech;
-            }
+        if (weatherData.cond_txt && pm25Data.pm25) {
+            const speech = `今天${city}天气${weatherData.cond_txt},${weatherData.wind_dir}${weatherData.wind_sc}级,温度${weatherData.tmp}摄氏度,PM25:${pm25Data.pm25},空气质量${pm25Data.qlty}.`;
+            response.speech = response.displayText = speech;
         }
     }
 }
